38d76f2658bf135403eb32013deee5762a223d37
DubstepKnight <nursmaster@gmail.com>
2020-03-21 18:56:49 +0200

    Exercise 2 is done

--- app/src/main/java/image/color/git/lab22and3/MainActivity.java
+++ app/src/main/java/image/color/git/lab22and3/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +15,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 
-        LinearLayout btnLayout = new LinearLayout(this);
+//     Creating layouts programmatically
+        LinearLayout verticalLayout = new LinearLayout(this);
+        LinearLayout horizontalLayout = new LinearLayout(this);
+//     Setting their orientations
+        verticalLayout.setOrientation(LinearLayout.VERTICAL);
+        horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+//     Creating buttons programmatically
         Button addBtn = new Button(this);
+        Button editBtn = new Button(this);
+        Button removeBtn = new Button(this);
+//     Setting text to the buttons
         addBtn.setText("Add");
-//    addBtn.setText("Nothing");
-        btnLayout.addView(addBtn);
+        editBtn.setText("Edit");
+        removeBtn.setText("Remove");
+//     Creating EditText programmatically
+        EditText editText = new EditText(this);
+//     Creating ListView programmatically
+        ListView listView = new ListView(this);
+//     Adding UI elements to the layouts
+        verticalLayout.addView(horizontalLayout);
+        horizontalLayout.addView(addBtn);
+        horizontalLayout.addView(editBtn);
+        horizontalLayout.addView(removeBtn);
+        verticalLayout.addView(editText);
+        verticalLayout.addView(listView);
 
-        setContentView(addBtn);
+        setContentView(verticalLayout);
     }
 
 
